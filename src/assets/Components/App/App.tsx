abf3fbf3264877f5ad3c6df3485a356ade8a2ef7
Oleksandr Baranov <sashok1987nk@gmail.com>
2026-02-27 11:40:31 +0100

    update All

--- src/assets/Components/App/App.tsx
+++ src/assets/Components/App/App.tsx
@@ -1,31 +1,60 @@
+import SearchBar from "../SearchBar/SearchBar";
+import css from "./App.module.css";
+import { fetchMovies } from "../../services/movieServices";
 import { useState } from "react";
+import type { Movie } from "../../Types/movie";
+import toast from "react-hot-toast";
+import { MovieGrid } from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+import { Toaster } from "react-hot-toast";
 
-import viteLogo from "/vite.svg";
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [movie, setMovie] = useState<Movie | null>(null);
 
-function App() {
-  const [count, setCount] = useState(0);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setMovie(null);
+  };
+  const onSelect = (movie: Movie) => {
+    setMovie(movie);
+    openModal();
+  };
 
+  const handleMovie = async (query: string) => {
+    try {
+      setMovies([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchMovies(query);
+      if (data.length > 0) {
+        setMovies(data);
+      } else {
+        toast.error("No movies found for your request.");
+      }
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={css.app}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <SearchBar onSubmit={handleMovie} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {movies.length > 0 && <MovieGrid movies={movies} onSelect={onSelect} />}
+      {isModalOpen && movie && (
+        <MovieModal onClose={closeModal} movie={movie} />
+      )}
+    </div>
   );
 }
-
-export default App;
---
+++ src/assets/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,48 @@
+import toast from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+interface onSubmit {
+  onSubmit: (query: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: onSubmit) {
+    const handleSubmit = (FormData: FormData): void => {
+        const query = String(FormData.get("query") ?? "").trim();
+        if (!query) {
+            toast.error("Please enter your a search query.");
+            return;
+        }
+        onSubmit(query);
+    };
+
+    return (
+        <header className={css.header}>
+  <div className={css.container}>
+    <a
+      className={css.link}
+      href="https://www.themoviedb.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by TMDB
+    </a>
+    <form className={css.form}>
+      <input
+        className={css.input}
+        type="text"
+        name="query"
+        autoComplete="off"
+        placeholder="Search movies..."
+        autoFocus
+      />
+      <button className={css.button} type="submit">
+        Search
+      </button>
+    </form>
+  </div>
+</header>
+    );
+}
+
+
+
