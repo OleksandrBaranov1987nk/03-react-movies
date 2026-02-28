8378e5167a4bfbcf52e9d1a18831c19e0181c4dd
Oleksandr Baranov <sashok1987nk@gmail.com>
2026-02-28 10:04:37 +0100

    update

--- src/assets/services/movieServices.ts
+++ src/assets/services/movieServices.ts
@@ -5,11 +5,16 @@ interface MovieResponse {
   results: Movie[];
 }
 
-
 const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
-const BASE_URL = "https://api.themoviedb.org/search/movie";
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
 export async function fetchMovies(query: string): Promise<Movie[]> {
+  if (!API_TOKEN) {
+    throw new Error(
+      "VITE_TMDB_TOKEN is missing. Add it to .env and Vercel env vars.",
+    );
+  }
+
   const res = await axios.get<MovieResponse>(BASE_URL, {
     params: {
       query,
@@ -17,10 +22,11 @@ export async function fetchMovies(query: string): Promise<Movie[]> {
       language: "en-US",
       page: 1,
     },
-
     headers: {
+      accept: "application/json",
       Authorization: `Bearer ${API_TOKEN}`,
     },
   });
+
   return res.data.results;
 }
